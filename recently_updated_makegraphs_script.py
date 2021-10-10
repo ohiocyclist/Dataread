e80bb8dd30910ca9c473446d5e65a2d6b7190e65
Benjamin Buck <91391981+ohiocyclist@users.noreply.github.com>
2021-10-10 14:11:52 -0600

    This file had an include name wrong

--- recently_updated_makegraphs_script.py
+++ recently_updated_makegraphs_script.py
@@ -5,16 +5,16 @@ import decimal
 import pandas as pd
 import numpy as np
 
-import probplot_trendplot
+import probplot
 
 def oneprobplot(onelot, thiscol, directory, makethismap=True, uslref=False,
                 lslref=False, withboxplots=True, keepplot=False,
                 splitcol="Split", **kwargs):
     
     (thisplt, isok, scaletop, scalebot, ppax) = \
-        probplot_trendplot.probplot(onelot, thiscol, splitcol,
-                                    connect_points=True,
-                                    withboxplots=withboxplots, **kwargs)
+        probplot.probplot(onelot, thiscol, splitcol,
+                          connect_points=True,
+                          withboxplots=withboxplots, **kwargs)
         
         # skip some stuff for adding lsl/usl reference lines
         
